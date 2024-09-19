2976680b29da92b5d12a5a55605ac9562488a5f6
Espen Sørum <111789263+esoerum@users.noreply.github.com>
2024-09-19 13:53:35 +0200

    Ferdig forelesning

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,3 +1,4 @@
+import AddStudentForm from "./AddStudentForm";
 import Student from "./Student";
 import { StudentProps } from "./types";
 import { useState } from "react";
@@ -9,13 +10,20 @@ type GridProps = {
 export default function Grid(props: GridProps) {
 	const { studentList } = props;
 	//State for å håndtere studentdata
-	const [students, setStudents] = useState(props.studentList ?? []);
+	const [students, setStudents] = useState<StudentProps[]>(props.studentList ?? []);
+
+	const onAddStudent = (student: Pick<StudentProps, "name">) => {
+		setStudents((prev) => [...prev, { id: crypto.randomUUID(), name: student.name }]);
+	}
     
 	return (
+		<section>
 		<article className="grid">
 			{students.map((student) => (
 				<Student key={student.id} id={student.id} name={student.name} />
 			))}
 		</article>
+		<AddStudentForm onAddStudent={onAddStudent}/>
+		</section>
 	);
 }
